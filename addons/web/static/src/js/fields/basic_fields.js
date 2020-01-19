09e0cf6ec2ca4c84a9a178db3bab18aa6d108c44
Gustavo Adrian Marino <gamarino@numaes.com>
2020-01-18 21:07:08 -0300

    [FIX] set max upload file size to 100Mb

--- addons/web/static/src/js/fields/basic_fields.js
+++ addons/web/static/src/js/fields/basic_fields.js
@@ -1411,7 +1411,7 @@ var AbstractFieldBinary = AbstractField.extend({
         this._super.apply(this, arguments);
         this.fields = record.fields;
         this.useFileAPI = !!window.FileReader;
-        this.max_upload_size = 25 * 1024 * 1024; // 25Mo
+        this.max_upload_size = 100 * 1024 * 1024; // 25Mo
         if (!this.useFileAPI) {
             var self = this;
             this.fileupload_id = _.uniqueId('o_fileupload');
