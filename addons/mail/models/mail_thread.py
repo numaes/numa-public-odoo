2806a390be4cd018cf93c7071ca04178d7408612
Gustavo Adrian Marino <gamarino@numaes.com>
2020-01-30 17:06:21 -0300

    [FIX] in case of incoming mails, calling message_new could lead to a non valid thread, returning a False/None. This should be considered and no process for the incoming mail

--- addons/mail/models/mail_thread.py
+++ addons/mail/models/mail_thread.py
@@ -1037,8 +1037,11 @@ class MailThread(models.AbstractModel):
                 # if a new thread is created, parent is irrelevant
                 message_dict.pop('parent_id', None)
                 thread = ModelCtx.message_new(message_dict, custom_values)
-                thread_id = thread.id
-                subtype_id = thread._creation_subtype().id
+                if thread:
+                    thread_id = thread.id
+                    subtype_id = thread._creation_subtype().id
+                else:
+                    return None
 
             # replies to internal message are considered as notes, but parent message
             # author is added in recipients to ensure he is notified of a private answer
