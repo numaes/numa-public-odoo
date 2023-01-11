odoo/odoo

[FIX] sale: Partial payment link with invoice policy based on deliverd qty
- Configure payment stripe to be available on the web shop
- Let's consider a product with P (sales price = 100€) invoice policy based on delivered qty
- Set in the config parameter sale.automatic_invoice to True
- Create a SO with P and generate a payment link PL for 50€
- Go to PL and pay it

Bug:

An error was raised because there was no invoicable line when computing the
invoice of SO

opw:2635175

addons/sale/tests/test_sale_transaction.py
@@ -58,3 +58,17 @@ class TestSaleTransaction(AccountTestInvoicingCommon):
         with mute_logger('odoo.addons.sale.models.payment'):
             self.transaction._post_process_after_done()
         self.assertEqual(self.order.state, 'draft', 'a transaction for an incorrect amount should not validate a quote')
+
+    def test_sale_transaction_partial_delivery(self):
+        """Test that with automatic invoice and invoicing policy based on delivered quantity, a transaction for the partial
+        amount does not validate the SO."""
+        # set automatic invoice
+        self.env['ir.config_parameter'].sudo().set_param('sale.automatic_invoice', 'True')
+        # modify order total
+        self.order.order_line[0].price_unit = 200.0
+        # invoicing policy is based on delivered quantity
+        self.product_a.invoice_policy = 'delivery'
+        self.transaction._set_transaction_done()
+        with mute_logger('odoo.addons.sale.models.payment'):
+            self.transaction.sudo()._post_process_after_done()
+        self.assertEqual(self.order.state, 'draft', 'a partial transaction with automatic invoice and invoice_policy = delivery should not validate a quote')

numaes/numa-public-odoo

Backport of dee53d8aecd3d1eedd37bfa93c07ca28a3441fbe
closes odoo/odoo#94438
Signed-off-by: Valentin Chevalier <vcr@odoo.com>

addons/sale/tests/test_sale_transaction.py
@@ -55,3 +55,17 @@ class TestSaleTransaction(AccountingTestCase):
         with mute_logger('odoo.addons.sale.models.payment'):
             self.transaction._post_process_after_done()
         self.assertEqual(self.order.state, 'draft', 'a transaction for an incorrect amount should not validate a quote')
+
+    def test_sale_transaction_partial_delivery(self):
+        """Test that with automatic invoice and invoicing policy based on delivered quantity, a transaction for the partial
+        amount does not validate the SO."""
+        # set automatic invoice
+        self.env['ir.config_parameter'].sudo().set_param('sale.automatic_invoice', 'True')
+        # modify order total
+        self.order.order_line[0].price_unit = 200.0
+        # invoicing policy is based on delivered quantity
+        self.product.invoice_policy = 'delivery'
+        self.transaction._set_transaction_done()
+        with mute_logger('odoo.addons.sale.models.payment'):
+            self.transaction.sudo()._post_process_after_done()
+        self.assertEqual(self.order.state, 'draft', 'a partial transaction with automatic invoice and invoice_policy = delivery should not validate a quote')