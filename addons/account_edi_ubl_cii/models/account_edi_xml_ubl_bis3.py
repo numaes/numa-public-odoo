321d4f70e0390ddfd186be76db55d55df9330583
Julien Van Roy <juvr@odoo.com>
2024-01-26 12:03:04 +0100

    [FIX] account_edi_ubl_cii: PartyTaxScheme should begin with country code
    In Peppol Bis 3, the value on PartyTaxScheme/CompanyID should be
    prefixed with a 2 letters code representing the country of the partner.
    Currently, filling a VAT without this country code will lead to the
    error: "[BR-CO-09]-The Seller VAT identifier (BT-31), the Seller tax
    representative VAT identifier (BT-63) and the Buyer VAT identifier
    (BT-48) shall have a prefix in accordance with ISO code ISO 3166-1
    alpha-2 by which the country of issue may be identified. Nevertheless,
    Greece may use the prefix ‘EL’."
    
    On the other hand, some extra-european countries use Peppol Bis 3 but do
    not prepend their VAT with the country code (i.e. Australia). To allow
    such cases, we automatically add the country code when it's missing.
    
    To prevent regression, we also add a contraint mimicking the schematron:
    we check that the final value of the PartyTaxScheme/CompanyID is
    prefixed with a 2 letters code. This way, the constraint is independant
    from the code.
    
    X-original-commit: 6d0b21fd168b8ac58b1f4edcc30ffa19cbf397b8
    Part-of: odoo/odoo#154076

--- addons/account_edi_ubl_cii/models/account_edi_xml_ubl_bis3.py
+++ addons/account_edi_ubl_cii/models/account_edi_xml_ubl_bis3.py
@@ -62,6 +62,7 @@ class AccountEdiXmlUBLBIS3(models.AbstractModel):
             if (
                 partner.country_id
                 and partner.country_id not in self.env.ref('base.europe').country_ids
+                and len(partner.vat) >= 2
                 and not partner.vat[:2].isalpha()
             ):
                 vals['company_id'] = partner.country_id.code + partner.vat
@@ -114,11 +115,11 @@ class AccountEdiXmlUBLBIS3(models.AbstractModel):
         vals['endpoint_id'] = partner.vat
         vals['endpoint_id_attrs'] = {'schemeID': COUNTRY_EAS.get(partner.country_id.code)}
 
-        if partner.country_code == 'NO':
-            if 'l10n_no_bronnoysund_number' in partner._fields:
-                vals['endpoint_id'] = partner.l10n_no_bronnoysund_number
-            else:
-                vals['endpoint_id'] = partner.vat.replace("NO", "").replace("MVA", "")
+        if partner.country_code == 'NO' and 'l10n_no_bronnoysund_number' in partner._fields:
+            vals.update({
+                'endpoint_id': partner.l10n_no_bronnoysund_number,
+                'endpoint_id_attrs': {'schemeID': '0192'},
+            })
         # [BR-NL-1] Dutch supplier registration number ( AccountingSupplierParty/Party/PartyLegalEntity/CompanyID );
         # With a Dutch supplier (NL), SchemeID may only contain 106 (Chamber of Commerce number) or 190 (OIN number).
         # [BR-NL-10] At a Dutch supplier, for a Dutch customer ( AccountingCustomerParty ) the customer registration
@@ -142,8 +143,6 @@ class AccountEdiXmlUBLBIS3(models.AbstractModel):
             })
         if partner.country_id.code == "LU" and 'l10n_lu_peppol_identifier' in partner._fields and partner.l10n_lu_peppol_identifier:
             vals['endpoint_id'] = partner.l10n_lu_peppol_identifier
-        if partner.country_id.code == "SE" and partner.vat:
-            vals['endpoint_id'] = partner.vat.replace("SE", "")[:-2]
 
         return vals
 
@@ -226,6 +225,8 @@ class AccountEdiXmlUBLBIS3(models.AbstractModel):
 
         for vals in vals_list:
             vals.pop('name')
+            # [UBL-CR-601]-A UBL invoice should not include the InvoiceLine Item ClassifiedTaxCategory TaxExemptionReason
+            #vals.pop('tax_exemption_reason')
 
         return vals_list
 
@@ -240,17 +241,6 @@ class AccountEdiXmlUBLBIS3(models.AbstractModel):
                 subtotal_vals['currency_dp'] = 2
 
         return vals_list
-
-    def _get_invoice_line_item_vals(self, line, taxes_vals):
-        # EXTENDS account.edi.xml.ubl_21
-        line_item_vals = super()._get_invoice_line_item_vals(line, taxes_vals)
-
-        for val in line_item_vals['classified_tax_category_vals']:
-            # [UBL-CR-601] TaxExemptionReason must not appear in InvoiceLine Item ClassifiedTaxCategory
-            # [BR-E-10] TaxExemptionReason must only appear in TaxTotal TaxSubtotal TaxCategory
-            val.pop('tax_exemption_reason')
-
-        return line_item_vals
 
     def _get_invoice_line_allowance_vals_list(self, line, tax_values_list=None):
         # EXTENDS account.edi.xml.ubl_21
@@ -385,13 +375,10 @@ class AccountEdiXmlUBLBIS3(models.AbstractModel):
                 constraints.update({'cen_en16931_tax_line': _("Each invoice line shall have one and only one tax.")})
 
         for role in ('supplier', 'customer'):
-            constraints[f'cen_en16931_{role}_country'] = self._check_required_fields(vals[role], 'country_id')
             scheme_vals = vals['vals'][f'accounting_{role}_party_vals']['party_vals']['party_tax_scheme_vals'][-1:]
-            if (
-                not (scheme_vals and scheme_vals[0]['company_id'] and scheme_vals[0]['company_id'][:2].isalpha())
-                and (scheme_vals and scheme_vals[0]['tax_scheme_id'] == 'VAT')
-                and self._name in ('account.edi.xml.ubl_bis3', 'account.edi.xml.ubl_nl', 'account.edi.xml.ubl_de')
-            ):
+            if not (scheme_vals and scheme_vals[0]['company_id'] and scheme_vals[0]['company_id'][:2].isalpha()) \
+                and (scheme_vals and scheme_vals[0]['tax_scheme_id'] == 'VAT') \
+                and self._name in ('account.edi.xml.ubl_bis3', 'account.edi.xml.ubl_nl', 'account.edi.xml.ubl_de'):
                 # [BR-CO-09]-The Seller VAT identifier (BT-31), the Seller tax representative VAT identifier (BT-63)
                 # and the Buyer VAT identifier (BT-48) shall have a prefix in accordance with ISO code ISO 3166-1
                 # alpha-2 by which the country of issue may be identified. Nevertheless, Greece may use the prefix ‘EL’.
