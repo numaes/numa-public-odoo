odoo/odoo

[IMP] discuss: upgrade to SFU as fallback for p2p failures

addons/mail/controllers/discuss/rtc.py
@@ -88,6 +88,14 @@ class RtcController(http.Controller):
         # sudo: discuss.channel.rtc.session - member of current user can leave call
         member.sudo()._rtc_leave_call(session_id)
 
+    @http.route("/mail/rtc/channel/upgrade", methods=["POST"], type="jsonrpc", auth="user")
+    def channel_upgrade(self, channel_id):
+        member = request.env["discuss.channel.member"].search([("channel_id", "=", channel_id), ("is_self", "=", True)])
+        if not member:
+            raise NotFound()
+        # sudo: discuss.channel.rtc.session - member of current user can upgrade channel
+        member.sudo()._join_sfu(force=True)
+
     @http.route("/mail/rtc/channel/cancel_call_invitation", methods=["POST"], type="jsonrpc", auth="public")
     @add_guest_to_context
     def channel_call_cancel_invitation(self, channel_id, member_ids=None):

numaes/numa-public-odoo

This commit makes it so that in the case of unsuccessful peer-to-peer
connections, the client will request to the Odoo server to upgrade
the channel with a SFU.
backport of: odoo/odoo#200122 and odoo/odoo#206411
closes odoo/odoo#228563
Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

addons/mail/controllers/discuss/rtc.py
@@ -88,6 +88,13 @@ class RtcController(http.Controller):
         # sudo: discuss.channel.rtc.session - member of current user can leave call
         member.sudo()._rtc_leave_call()
 
+    @http.route("/mail/rtc/channel/upgrade_connection", methods=["POST"], type="json", auth="user")
+    def channel_upgrade(self, channel_id):
+        member = request.env["discuss.channel.member"].search([("channel_id", "=", channel_id), ("is_self", "=", True)])
+        if not member:
+            raise NotFound()
+        member.sudo()._join_sfu(force=True)
+
     @http.route("/mail/rtc/channel/cancel_call_invitation", methods=["POST"], type="json", auth="public")
     @add_guest_to_context
     def channel_call_cancel_invitation(self, channel_id, member_ids=None):