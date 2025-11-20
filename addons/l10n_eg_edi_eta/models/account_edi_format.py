1b5785ec0bd2c765c238d3ea310f14981481f118
Ahmed Abdelaziz Hasaneen <aaha@odoo.com>
2025-11-20 10:53:17 +0000

    [FIX] l10n_eg_edi_eta: fix JSONDecodeError catching
    Before this commit:
    Steps
    1) When clients try to download e-invoice for ETA
    2) If ETA rejects the request, Odoo fails to parse to JSON
    3) a JSONDecodeError exception is raised
    4) Odoo doesn't catch it and a traceback is raised
    
    => A JSONDecodeError is raised but actually it's not json.decoder.JSONDecodeError, it's actually requests.exceptions.JSONDecodeError as mentioned here https://requests.readthedocs.io/en/latest/api/#requests.JSONDecodeError
    
    After this commit:
    If the request is rejected and Odoo failed to parse the response to JSON the exception is catched properly.
    
    opw-5241411
    opw-5272195
    
    closes odoo/odoo#236750
    
    X-original-commit: 625320926d13d998a372cf464a10dd2eb3657e85
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- addons/l10n_eg_edi_eta/models/account_edi_format.py
+++ addons/l10n_eg_edi_eta/models/account_edi_format.py
@@ -57,7 +57,7 @@ class AccountEdiFormat(models.Model):
         if not request_response.ok:
             try:
                 response_data = request_response.json()
-            except JSONDecodeError as ex:
+            except requests.exceptions.JSONDecodeError as ex:
                 return {
                     'error': str(ex),
                     'blocking_level': 'error'
