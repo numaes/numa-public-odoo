f033c7a30a4349447f88038e14c1929cc05c9979
Louis Travaux <lotr@odoo.com>
2025-11-21 13:46:47 +0000

    [FIX] hw_drivers: prevent duplicate report printing
    In order to prevent duplicate IoT actions, we now ensure that websocket
    actions have not already been called through longpolling.
    
    odoo/enterprise#100161
    
    closes odoo/odoo#238575
    
    X-original-commit: aa66f20af0eaf132785fea26abfdff248a5bcfb9
    Related: odoo/enterprise#101257
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>
    Signed-off-by: Louis Travaux (lotr) <lotr@odoo.com>

--- addons/hw_drivers/controllers/driver.py
+++ addons/hw_drivers/controllers/driver.py
@@ -37,10 +37,9 @@ class DriverController(http.Controller):
             # Skip the request if it was already executed (duplicated action calls)
             if iot_device._check_idempotency(**data, session_id=session_id):
                 return False
-            start_operation_time = time.perf_counter()
-            _logger.info("Longpolling: calling action %s for device %s", data.get('action', ''), device_identifier)
+
+            _logger.debug("Calling action %s for device %s", data.get('action', ''), device_identifier)
             iot_device.action(data)
-            _logger.info("device '%s' action finished - %.*f", device_identifier, 3, time.perf_counter() - start_operation_time)
             return True
         return False
 
